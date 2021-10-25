bd37de687e0c75bd5f84e1df9f68ca90e2fe5bd0
shilpisarkar <72228323+shilpisarkar@users.noreply.github.com>
2021-10-25 18:48:13 +0530

    Create insertionSort.c

---
+++ insertionSort.c
@@ -0,0 +1,90 @@
+/*
+    ALGORITHM :
+
+Step 1 − If it is the first element, it is already sorted. return 1;
+Step 2 − Pick next element
+Step 3 − Compare with all elements in the sorted sub-list
+Step 4 − Shift all the elements in the sorted sub-list that is greater than the 
+         value to be sorted
+Step 5 − Insert the value
+Step 6 − Repeat until list is sorted
+
+*/
+
+#include<stdio.h>
+
+void printArray(int* A, int n){
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+}
+
+void insertionSort(int *A, int n){
+    int key, j;
+    // Loop for passes
+    for (int i = 1; i <= n-1; i++)
+    {
+        key = A[i];
+        j = i-1;
+        // Loop for each pass
+        while(j>=0 && A[j] > key){
+            A[j+1] = A[j];
+            j--;
+        }
+        A[j+1] = key;
+    }
+}
+
+void insertionSort2(int A[], int n){
+    for (int i = 1; i<n-1; i++){
+        int key = A[i];
+        int j = i - 1;
+        while(j>=0 && A[j] > key){
+            A[j+1] = A[j];
+            j--;
+        }
+        A[j+1] = key;
+    }
+    
+}
+
+int main(){
+   // -1   0    1   2   3   4   5
+   //      12,| 54, 65, 07, 23, 09 --> i=1, key=54, j=0
+   //      12,| 54, 65, 07, 23, 09 --> 1st pass done (i=1)!
+
+   //      12, 54,| 65, 07, 23, 09 --> i=2, key=65, j=1
+   //      12, 54,| 65, 07, 23, 09 --> 2nd pass done (i=2)!
+
+   //      12, 54, 65,| 07, 23, 09 --> i=3, key=7, j=2
+   //      12, 54, 65,| 65, 23, 09 --> i=3, key=7, j=1
+   //      12, 54, 54,| 65, 23, 09 --> i=3, key=7, j=0
+   //      12, 12, 54,| 65, 23, 09 --> i=3, key=7, j=-1
+   //      07, 12, 54,| 65, 23, 09 --> i=3, key=7, j=-1--> 3rd pass done (i=3)!
+
+   // Fast forwarding and 4th and 5th pass will give:
+   //      07, 12, 54, 65,| 23, 09 --> i=4, key=23, j=3
+   //      07, 12, 23, 54,| 65, 09 --> After the 4th pass
+
+   //      07, 12, 23, 54, 65,| 09 --> i=5, key=09, j=4
+   //      07, 09, 12, 23, 54, 65| --> After the 5th pass 
+    
+    int A[] = {12, 54, 65, 7, 23, 9};
+    int n = 6;
+    printArray(A, n);
+    insertionSort2(A, n);
+    printArray(A, n);
+    return 0;
+}
+
+/*
+Worst Case Time Complexity [ Big-O ]: O(n2)
+
+Best Case Time Complexity [Big-omega]: O(n)
+
+Average Time Complexity [Big-theta]: O(n2)
+
+Space Complexity: O(1)
+*/
